d12e27486b0348e78fbd7833970564118a178e44
elasticMongrel <33241429+elasticMongrel@users.noreply.github.com>
2018-02-25 21:00:44 -0500

    Update Square.java
    Put in changed I worked on

--- Square.java
+++ Square.java
@@ -1,34 +1,56 @@
-# Minesweeper
-Minesweeper game for AP CompSci
-
 public class Square{
 	boolean isBomb;
 	int bombNum; //number of bombs touching tile
 	int num; //number of that individual square
 	
-	public Square(boolean a, int b, int c){
+	public Square(boolean a, int b, int c){ //constructor
 		isBomb = a;
 		bombNum = b;
 		num = c;
 	}
 	
-	@Override //overrides .equals()
-	public boolean equals(Object obj){
-	
+	public boolean bombCheck(Object obj){ //accessor to see if square is a bomb
+		return isBomb;
+		//what's the difference between doing object.bombCheck() vs bombCheck(object)??????
 	}
 	
-	@Override //overrides hashCode
-	public int hashCode(){
-	
-	}
-	
-	public void setBombNum(){ //sets square that aren't bombs
+	public void setBombNum(){ //bombNum modifier
 		for(int x = 0; x < 9; x++){
 			for(int y = 0; y < 9; y++){
-				if(Board.grid[x-1][y-1].equals((true, 0, 0))){
+				if(bombCheck(Board.sqrGrid[x-1][y-1]) == true){ //top left corner
+					bombNum++;
+				}
+				if(bombCheck(Board.sqrGrid[x][y-1]) == true){ //on top
+					bombNum++;
+				}
+				if(bombCheck(Board.sqrGrid[x+1][y-1]) == true){ //top right corner
+					bombNum++;
+				}
+				if(bombCheck(Board.sqrGrid[x-1][y]) == true){ //left
+					bombNum++;
+				}
+				if(bombCheck(Board.sqrGrid[x+1][y]) == true){ //right
+					bombNum++;
+				}
+				if(bombCheck(Board.sqrGrid[x-1][y+1]) == true){ //bottom left corner
+					bombNum++;
+				}
+				if(bombCheck(Board.sqrGrid[x][y+1]) == true){ //on bottom
+					bombNum++;
+				}
+				if(bombCheck(Board.sqrGrid[x+1][y+1]) == true){ //bottom right corner
 					bombNum++;
 				}
 			}
 		}
+	}
+	
+	public boolean makeBomb(){ //modifier that makes isBomb true
+		return isBomb = true;
+	}
+	
+	@Override //to look at actually values of the squares/debug
+	public String toString(){
+		return "(isBomb = " + isBomb + "; bombNum = " + bombNum + "; num = " + num +")";
 	}
 }
